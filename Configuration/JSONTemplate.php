fdee2faca3a1c9cbf3a3095d366005eeb1db1dc8
Eric GELOEN <geloen.eric@gmail.com>
2012-02-25 15:40:02 +0100

    Create JSONTemplate annotation class

---
+++ Configuration/JSONTemplate.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Widop package.
+ *
+ * (c) Widop <contact@widop.com>
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Widop\FrameworkExtraBundle\Configuration;
+
+/**
+ * JSON template annotation.
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class JSONTemplate
+{  
+}
