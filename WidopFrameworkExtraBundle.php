84606eab283160f47db779ce16926c2d1d4049e1
Eric GELOEN <geloen.eric@gmail.com>
2012-02-25 15:39:26 +0100

    Initial commit

---
+++ WidopFrameworkExtraBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Widop package.
+ *
+ * (c) Widop <contact@widop.com>
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Widop\FrameworkExtraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WidopFrameworkExtraBundle extends Bundle
+{
+}
